c889e3d533778b6b78704c136b8470227f3eae2c
edujgurjc <26252085+EduJGURJC@users.noreply.github.com>
2019-07-29 15:29:30 +0200

    +

--- src/test/java/io/elastest/codeurjc/qe/openvidu/ElastestBaseTest.java
+++ src/test/java/io/elastest/codeurjc/qe/openvidu/ElastestBaseTest.java
@@ -1,28 +1,39 @@
 package io.elastest.codeurjc.qe.openvidu;
 
+import static java.lang.invoke.MethodHandles.lookup;
+import static org.slf4j.LoggerFactory.getLogger;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestInfo;
 import org.openqa.selenium.WebDriver;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.cloudformation.AmazonCloudFormation;
 import com.amazonaws.services.cloudformation.AmazonCloudFormationClient;
+import com.amazonaws.services.cloudformation.model.CreateStackRequest;
+import com.amazonaws.services.cloudformation.model.DeleteStackRequest;
+import com.amazonaws.services.cloudformation.model.DescribeStackResourcesRequest;
+import com.amazonaws.services.cloudformation.model.DescribeStacksRequest;
+import com.amazonaws.services.cloudformation.model.Output;
+import com.amazonaws.services.cloudformation.model.Stack;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class ElastestBaseTest {
-    protected static final Logger logger = LoggerFactory
-            .getLogger(ElastestBaseTest.class);
+    protected static final Logger logger = getLogger(lookup().lookupClass());
 
     protected static String eusURL;
     protected static String openviduSutUrl;
@@ -33,9 +44,11 @@ public class ElastestBaseTest {
     protected static Map<String, Object> awsConfig;
 
     protected static AmazonCloudFormation awsCloudFormation;
+    protected static String stackName = "OpenViduWebAppStack";
+    protected static String cloudFormationFile;
 
     @BeforeAll
-    public static void setupClass() {
+    public static void setupClass() throws Exception {
         driverList = new ArrayList<>();
 
         /* *********************************** */
@@ -119,6 +132,9 @@ public class ElastestBaseTest {
         /* *********************************** */
         // TODO
 
+        cloudFormationFile = System
+                .getenv("OPENVIDU_WEBAPP_CLOUDFORMATION_PATH");
+
         BasicAWSCredentials credentials = new BasicAWSCredentials(accessKeyId,
                 secretAccessKey);
         AWSStaticCredentialsProvider credentialsProvider = new AWSStaticCredentialsProvider(
@@ -127,8 +143,29 @@ public class ElastestBaseTest {
         awsCloudFormation = AmazonCloudFormationClient.builder()
                 .withRegion(region).withCredentials(credentialsProvider)
                 .build();
-        
-//        awsCloudFormation.dep
+
+        CreateStackRequest createRequest = new CreateStackRequest();
+        createRequest.setStackName(stackName);
+        createRequest
+                .setTemplateBody(convertStreamToString(ElastestBaseTest.class
+                        .getResourceAsStream(cloudFormationFile)));
+        logger.info("Creating a stack called " + createRequest.getStackName()
+                + ".");
+        awsCloudFormation.createStack(createRequest);
+
+        for (Stack stack : awsCloudFormation
+                .describeStacks(new DescribeStacksRequest()).getStacks()) {
+            DescribeStackResourcesRequest stackResourceRequest = new DescribeStackResourcesRequest();
+            stackResourceRequest.setStackName(stack.getStackName());
+
+            for (Output output : stack.getOutputs()) {
+                if (output.getOutputKey() == "WebsiteURL") {
+                    // TODO
+                } else if (output.getOutputKey() == "WebsiteURLLE") {
+                    // TODO
+                }
+            }
+        }
 
         // openviduWebAppUrl =
 
@@ -156,4 +193,25 @@ public class ElastestBaseTest {
         driverList = new ArrayList<>();
     }
 
+    @AfterAll
+    public void clear() {
+        if (awsCloudFormation != null) {
+            logger.info("Deleting webapp stack instance");
+            DeleteStackRequest deleteRequest = new DeleteStackRequest();
+            deleteRequest.setStackName(stackName);
+            awsCloudFormation.deleteStack(deleteRequest);
+        }
+    }
+
+    public static String convertStreamToString(InputStream in)
+            throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+        StringBuilder stringbuilder = new StringBuilder();
+        String line = null;
+        while ((line = reader.readLine()) != null) {
+            stringbuilder.append(line + "\n");
+        }
+        in.close();
+        return stringbuilder.toString();
+    }
 }
